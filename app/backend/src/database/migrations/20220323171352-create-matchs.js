4a59aa1627e535941cf5221c26dc36e3ec734314
Gabriel Sampaio Bicalho <gabrielsampaiobicalho@gmail.com>
2022-03-23 14:26:57 -0300

    implements migration match

---
+++ app/backend/src/database/migrations/20220323171352-create-matchs.js
@@ -0,0 +1,43 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('matchs', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      home_team: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'clubs',
+          key: 'id',
+        },
+      },
+      home_team_goals: {
+        type: Sequelize.INTEGER,
+      },
+      away_team: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'clubs',
+          key: 'id',
+        },
+      },
+      away_team_goals: {
+        type: Sequelize.INTEGER,
+      },
+      inprogress: {
+        type: Sequelize.BOOLEAN
+      }
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('matchs');
+  }
+};
