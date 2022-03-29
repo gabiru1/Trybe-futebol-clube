c374ad2003125223ef0c0fd8171e4a8754d38ddd
Gabriel Sampaio Bicalho <gabrielsampaiobicalho@gmail.com>
2022-03-29 19:41:50 -0300

    req 5 7 9 11 13

--- app/backend/src/middlewares/isValidPassword.ts
+++ app/backend/src/middlewares/isValidPassword.ts
@@ -3,6 +3,8 @@ import { NextFunction, Request, Response } from 'express';
 const isValidPassword = (req: Request, res: Response, next: NextFunction) => {
   const { password } = req.body;
 
+  if (!password) return res.status(401).json({ message: 'All fields must be filled' });
+
   if (password.length < 6) return res.status(401).json({ message: 'Incorrect email or password' });
 
   next();
--- app/backend/src/service/login.ts
+++ app/backend/src/service/login.ts
@@ -3,18 +3,21 @@ import Users from '../database/models/Users';
 import LoginInterface from '../interfaces/LoginInterface';
 import UserInterface from '../interfaces/UserInterface';
 
+const messageFilled = 'All fields must be filled';
+const messageIncorrect = 'Incorrect email or password';
+
 const verifyUserService = async ({ email, password }: LoginInterface): Promise<UserInterface> => {
+  if (!email) throw new Error(messageFilled);
+
+  if (!password) throw new Error(messageFilled);
+
   const validUser = await Users.findOne({ where: { email } });
 
-  if (validUser === null) throw new Error('Incorrect email or password');
-
-  if (!email) throw new Error('All fields must be filled');
-
-  if (!password) throw new Error('All fields must be filled');
+  if (validUser === null) throw new Error(messageIncorrect);
 
   const validPassword = compareSync(password, validUser.password);
 
-  if (!validPassword) throw new Error('Incorrect email or password');
+  if (!validPassword) throw new Error(messageIncorrect);
 
   const { id, username, role } = validUser;
 
